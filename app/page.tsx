311f54c1dcd0749ddb6330face45b024a3a84f81
aayushsinha30 <sinhaaayush30@gmail.com>
2026-06-28 23:08:12 +0530

    Trigger Vercel redeploy for image display

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import PageMain from "./components/PageMain";
 
+// Vercel redeploy trigger - project images updated
 export default function Page(): React.ReactElement {
   return <PageMain />;
 }
